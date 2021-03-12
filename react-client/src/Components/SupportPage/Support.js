d6847036565b2580c625467744be339c3ba5cd2e
nikhilgwl <nikhilgwl0512@gmail.com>
2021-03-13 00:22:48 +0530

    Support Page folder created

---
+++ react-client/src/Components/SupportPage/Support.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from 'react-router';
+// import "./support.css";
+
+
+function Support() {
+
+    return (
+        <div className="supportbody">
+
+            <p>We here at CollegeSpace strive to provide you with the best campus experience in VJTI.
+            Here You can find all of your requisites at one single place. For any suggestions or
+            problems
+            </p>
+            <h3>
+                Contact us on our email
+            </h3>
+            <h4>
+                at
+            </h4>
+            <h3>
+                collegespacevjti@gmail.com
+            </h3>
+        </div>
+
+
+
+
+    );
+}
+export default Support;
+
